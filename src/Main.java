2811261d55864f6fea52d3da7feffb45cddcdcc0
raymondddenny <rrettob@gmail.com>
2023-10-27 23:46:34 +0700

    Finish linear, binary search and 1 example question

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+
+    public static Boolean linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Boolean binarySearch(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = (low + high) / 2;
+
+        while (low < high) {
+            if (arr[mid] < target) {
+                low = mid + 1;
+            } else if (arr[mid] > target) {
+                high = mid - 1;
+            } else if (arr[mid] == target) {
+                return true;
+            }
+            mid = (low + high) / 2;
+        }
+        return false;
+    }
+
+    public static int twoCrystalBreak(Boolean[] arr) {
+        int jumpAmnt = (int) Math.floor(Math.sqrt(arr.length));
+        System.out.println(jumpAmnt);
+
+        int i = jumpAmnt;
+
+        for (; i < arr.length; i += jumpAmnt) {
+            if (arr[i]) {
+                break;
+            }
+            System.out.println(i);
+        }
+        i -= jumpAmnt;
+        for (int j = 0; j <= jumpAmnt; ++j, ++i) {
+            if (arr[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+//        Boolean result = linearSearch(new int[]{1, 2, 3, 4, 5}, 3);
+//        System.out.println("Linear search - The target is on array ? " + result);
+//
+//        Boolean resultBinary = binarySearch(new int[]{1, 2, 3, 4, 5}, 3);
+//        System.out.println("Binary search - The target is on array ? " + resultBinary);
+
+        int resultCrystal = twoCrystalBreak(new Boolean[]{false, false, false, false, false, false, false, false, false, true, false, false, false, false, false});
+        System.out.println("Crystal search - The target is on array ? " + resultCrystal);
+
+    }
+}
